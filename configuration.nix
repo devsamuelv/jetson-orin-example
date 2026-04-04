2753ba7c2ed2f0c0b7f44ddca30cdd9d9493fdfd
devsamuelv <realalloutmusic@gmail.com>
2026-04-04 00:48:51 -0400

    ?

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,9 @@
 { ... } : {
+  # Just set these options to make the toplevel system evaluate without assertion errors
+  fileSystems."/".fsType = "tmpfs";
+  boot.loader.grub.enable = false;
+  boot.loader.systemd-boot.enable = false;
+
   hardware.nvidia-jetpack.enable = true;
   hardware.nvidia-jetpack.som = "orin-nano"; # Other options include orin-agx, xavier-nx, and xavier-nx-emmc
   hardware.nvidia-jetpack.super = true;
@@ -6,6 +11,18 @@
   hardware.nvidia.datacenter.enable = true;
   hardware.nvidia-jetpack.configureCuda = false;  # Disable CUDA config for cross-build
   networking.hostName = "curiosity";
+
+  services.create_ap = {
+    enable = true;
+    settings = {
+      INTERNET_IFACE = "eth0"; # Your internet-connected interface
+      WIFI_IFACE = "wlan0";    # Your wireless interface
+      SSID = "My Wifi Hotspot";
+      PASSPHRASE = "12345678";
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 22 ];
 
   nixpkgs.config.allowUnfree = true;
 
